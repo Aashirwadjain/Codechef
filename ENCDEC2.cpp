84c113d12b04a67260c03f6e1b3a6141e960e840
Aashirwad Jain <aashirwad2001jain@gmail.com>
2020-12-27 22:33:54 +0530

    Contest: Encoding December 2020

---
+++ ENCDEC2.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/ENDE2020/problems/ENCDEC2
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long int t;
+    cin >> t;
+    while(t--){
+        long long int R, C;
+        cin >> R >> C;
+        long long int k = R*C;
+        cout << k << endl;
+    }
+    return 0;
+}
